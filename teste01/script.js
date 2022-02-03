b289160f7996740edec3dc4f5001517d03da28f7
Nalbert Nascimento <96746029+nalbert14@users.noreply.github.com>
2022-02-03 11:16:37 -0300

    Adicionando meu primeiro arquivo no github
    teste 01

---
+++ teste01/script.js
@@ -0,0 +1,14 @@
+var traducao = document.getElementById('#btn1')
+
+function traduzir(a) {
+  if (a == '1') {
+    alert("Olá mundo")
+  }if (a === '2'){
+    alert("Meu nome é Nalbert!")
+  }if(a == '3')
+  alert("Estou aprendendo a programar!")
+  else if( a== '4')
+    alert("Foi o que eu consegui fazer em 30 min. kkk")
+  
+}
+
